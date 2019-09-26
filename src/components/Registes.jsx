fe20b41589d197359a9da0bc29a773fbc5a04669
dery <derydeviandi@gmail.com>
2019-09-26 11:05:28 +0700

    latihanreact2

---
+++ src/components/Registes.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Registes extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Registes
